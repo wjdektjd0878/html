a7f6fb54226c9911c090a88287f601620757304b
JungJung <77655831+wjdektjd0878@users.noreply.github.com>
2021-09-16 15:53:32 +0900

    Add files via upload

---
+++ php/db_con.php
@@ -0,0 +1,17 @@
+<?php
+    // mysqli
+    $db_host = "192.168.1.134";
+    $db_user = "itbank";
+    $db_pass = "itbank";
+    $db_name = "damem";
+    $conn = mysqli_connect('192.168.1.134','itbank','itbank','damem');
+    /*
+    if (!$conn) {
+        echo "<script>alert(\"DB Connection False\");</script>";
+    }
+    else {
+        echo "<script>alert(\"DB Connection Success\");</script>";
+    }
+    */
+?> 
+
